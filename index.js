f9f9fb49b253f8213456d4aba9da14b57f7ce5ea
Shafiul19 <shafiulhstu@gmail.com>
2024-03-03 17:26:54 +0600

    courses related api created

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
     res.send("Shafi Academi server is running");
   });
+
+app.get("/courses", (req,res) => {
+    res.send(courses);
+})
 
   app.listen(port, () => {
     console.log("Server is Running on Port", port);
